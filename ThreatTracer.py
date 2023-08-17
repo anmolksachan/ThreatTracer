460d1742c7522accde9443282cb76b93ed666a66
Anmol K Sachan <anmol221999@gmail.com>
2023-08-17 15:26:34 +0530

    Update ThreatTracer.py

--- ThreatTracer.py
+++ ThreatTracer.py
@@ -1,6 +1,6 @@
 import requests
+import re
 from termcolor import colored
-import urllib.parse
 
 art = """
   _______ _                    _ _______                      
@@ -9,53 +9,72 @@ art = """
     | |  | '_ \| '__/ _ \/ _` | __|| | '__/ _` |/ __/ _ \ '__|
     | |  | | | | | |  __/ (_| | |_ | | | | (_| | (_|  __/ |   
     |_|  |_| |_|_|  \___|\__,_|\__||_|_|  \__,_|\___\___|_|  
-  A Script to identify CVE by name & version by @FR13ND0x7F
+    A Script to identify CVE usinng CPE by name & version by @FR13ND0x7F
 """
 
-print(art)
+print(colored(art, "cyan"))
 
-def fetch_cve_details(component, version):
+def find_cpes(component, version):
+    base_url = "https://nvd.nist.gov/products/cpe/search/results"
+    params = {
+        "namingFormat": "2.3",
+        "keyword": f"{component} {version}"
+    }
+
+    response = requests.get(base_url, params=params)
+    print(f"URL Used: {response.url}")  # Print the URL used to find CPE
+    content = response.text
+
+    cpe_matches = re.findall(r'cpe:(.*?)<', content)
+    return cpe_matches
+
+def fetch_cve_details(cpe_strings):
     base_url = "https://services.nvd.nist.gov/rest/json/cves/1.0"
-    query = f"cpe:/a:{component}:{component}:{version}"
-    url = f"{base_url}?cpeMatchString={query}"
-    print(colored(f"Querying: {url}", "red"))
+    for cpe_string in cpe_strings:
+        cve_query_string = ":".join(cpe_string.split(":")[1:5])  # Extract relevant CPE part (vendor, product, version, update)
+        url = f"{base_url}?cpeMatchString=cpe:/{cve_query_string}"
+        print(colored(f"Querying: {url}", "red"))
 
-    response = requests.get(url)
-    data = response.json()
+        response = requests.get(url)
+        data = response.json()
 
-    if "result" in data:
-        cves = data["result"]["CVE_Items"]
-        print(colored("\nCVE Details\n", "cyan", attrs=["underline"]))
-        for cve_item in cves:
-            cve_id = cve_item["cve"]["CVE_data_meta"]["ID"]
-            description = cve_item["cve"]["description"]["description_data"][0]["value"]
-            link = f"https://nvd.nist.gov/vuln/detail/{cve_id}"
+        if "result" in data:
+            cves = data["result"]["CVE_Items"]
+            print(colored("\nCVE Details", "cyan", attrs=["underline"]))
+            for cve_item in cves:
+                cve_id = cve_item["cve"]["CVE_data_meta"]["ID"]
+                description = cve_item["cve"]["description"]["description_data"][0]["value"]
+                link = f"https://nvd.nist.gov/vuln/detail/{cve_id}"
 
-            weaknesses = []
-            if "problemtype" in cve_item["cve"]:
-                for problem_type in cve_item["cve"]["problemtype"]["problemtype_data"]:
-                    for description in problem_type["description"]:
-                        weaknesses.append(description["value"])
+                weaknesses = []
+                if "problemtype" in cve_item["cve"]:
+                    for problem_type in cve_item["cve"]["problemtype"]["problemtype_data"]:
+                        for description in problem_type["description"]:
+                            weaknesses.append(description["value"])
 
-            if "description_data" in cve_item["cve"]["description"]:
-                description_text = cve_item["cve"]["description"]["description_data"][0]["value"]
-            else:
-                description_text = "Description not available."
+                if "description_data" in cve_item["cve"]["description"]:
+                    description_text = cve_item["cve"]["description"]["description_data"][0]["value"]
+                else:
+                    description_text = "Description not available."
 
-            print(colored(f"CVE ID: {cve_id}", "red"))
-            print(colored(f"Description: {description_text}", "yellow"))
-            if weaknesses:
-                print(colored(f"Weaknesses: {', '.join(weaknesses)}", "magenta"))
-            print(colored(f"Link: {link}\n", "blue"))
+                print(colored(f"CVE ID: {cve_id}", "red"))
+                print(colored(f"Description: {description_text}", "yellow"))
+                if weaknesses:
+                    print(colored(f"Weaknesses: {', '.join(weaknesses)}", "magenta"))
+                print(colored(f"Link: {link}\n", "blue"))
 
 if __name__ == "__main__":
-    print(colored("CVE Checker Script", "green", attrs=["bold"]))
-    print(colored("This script fetches CVE details for a given component and version.\n", "white"))
+    print(colored("CPE Finder Script", "green", attrs=["bold"]))
+    print("This script searches for the CPEs of a component and version.\n")
 
     component = input(colored("Enter the component (e.g., jquery): ", "cyan"))
     version = input(colored("Enter the version (e.g., 1.0.0): ", "cyan"))
 
-    component_encoded = urllib.parse.quote(component)
-    version_encoded = urllib.parse.quote(version)
-
-    fetch_cve_details(component_encoded, version_encoded)
+    cpe_strings = find_cpes(component, version)
+    if cpe_strings:
+        print(colored("CPEs Found:", "green"))
+        for cpe_string in cpe_strings:
+            print(colored(f"  {cpe_string}", "green"))
+        fetch_cve_details(cpe_strings)
+    else:
+        print(colored("CPEs not found for the provided component and version.", "red"))
