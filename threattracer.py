1ac2267741e4f13301e6f8e26e5377c18f29ede2
Anmol K Sachan <anmol221999@gmail.com>
2023-08-17 16:40:08 +0530

    Update threattracer.py

--- threattracer.py
+++ threattracer.py
@@ -9,7 +9,7 @@ art = """
     | |  | '_ \| '__/ _ \/ _` | __|| | '__/ _` |/ __/ _ \ '__|
     | |  | | | | | |  __/ (_| | |_ | | | | (_| | (_|  __/ |   
     |_|  |_| |_|_|  \___|\__,_|\__||_|_|  \__,_|\___\___|_|  
-    A Script to identify CVE using CPE by name & version 
+    A Script to identify CVE usinng CPE by name & version 
         Credit: @FR13ND0x7F @0xCaretaker @meppohak5
 """
 
@@ -29,6 +29,15 @@ def find_cpes(component, version):
     cpe_matches = re.findall(r'cpe:(.*?)<', content)
     return cpe_matches
 
+def synk_db(cve_id):
+    res = requests.get(f"https://security.snyk.io/vuln/?search={cve_id}")
+    a_tag_pattern = r'data-snyk-test="vuln table title".*>([^"]+)<!----><!---->'
+    a_tag_matches = re.findall(a_tag_pattern, res.text)
+
+    if a_tag_matches:
+        snyk_short_name = a_tag_matches[0].lstrip().rstrip()
+        return snyk_short_name
+
 def fetch_cve_details(cpe_strings):
     base_url = "https://services.nvd.nist.gov/rest/json/cves/1.0"
     for cpe_string in cpe_strings:
@@ -44,6 +53,8 @@ def fetch_cve_details(cpe_strings):
             print(colored("\nCVE Details", "cyan", attrs=["underline"]))
             for cve_item in cves:
                 cve_id = cve_item["cve"]["CVE_data_meta"]["ID"]
+                snyk_short_name = synk_db(cve_id)
+                
                 description = cve_item["cve"]["description"]["description_data"][0]["value"]
                 link = f"https://nvd.nist.gov/vuln/detail/{cve_id}"
 
@@ -59,6 +70,8 @@ def fetch_cve_details(cpe_strings):
                     description_text = "Description not available."
 
                 print(colored(f"CVE ID: {cve_id}", "red"))
+                if snyk_short_name:
+                    print(colored(f"Short Name: {snyk_short_name}", "green"))
                 print(colored(f"Description: {description_text}", "yellow"))
                 if weaknesses:
                     print(colored(f"Weaknesses: {', '.join(weaknesses)}", "magenta"))
