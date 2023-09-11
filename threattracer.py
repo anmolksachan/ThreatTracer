d9d126903fbd7123cca9f142ef36cc9b380c5e64
Anmol K Sachan <anmol221999@gmail.com>
2023-09-11 12:33:38 +0530

    Update threattracer.py

--- threattracer.py
+++ threattracer.py
@@ -2,6 +2,8 @@ import requests
 import re
 from termcolor import colored
 from datetime import datetime
+import json
+from pyExploitDb import PyExploitDb
 
 art = """
   _______ _                    _ _______                      
@@ -9,7 +11,7 @@ art = """
     | |  | |__  _ __ ___  __ _| |_ | |_ __ __ _  ___ ___ _ __ 
     | |  | '_ \| '__/ _ \/ _` | __|| | '__/ _` |/ __/ _ \ '__|
     | |  | | | | | |  __/ (_| | |_ | | | | (_| | (_|  __/ |   
-    |_|  |_| |_|_|  \___|\__,_|\__||_|_|  \__,_|\___\___|_|  
+    |_|  |_| |_|_|  \___|\__,_|\__||_|_|  \__,_|\___\___|_|  Version 2.0
     A Script to identify CVE using CPE by name & version 
         Credit: @FR13ND0x7F @0xCaretaker @meppohak5
 """
@@ -39,46 +41,61 @@ def synk_db(cve_id):
         snyk_short_name = a_tag_matches[0].lstrip().rstrip()
         return snyk_short_name
 
-def fetch_cve_details(cpe_strings):
+def fetch_cve_details(cpe_string):
     base_url = "https://services.nvd.nist.gov/rest/json/cves/1.0"
     results = []
 
-    for cpe_string in cpe_strings:
-        cve_query_string = ":".join(cpe_string.split(":")[1:5])  # Extract relevant CPE part (vendor, product, version, update)
-        url = f"{base_url}?cpeMatchString=cpe:/{cve_query_string}"
+    cve_query_string = ":".join(cpe_string.split(":")[1:5])  # Extract relevant CPE part (vendor, product, version, update)
+    url = f"{base_url}?cpeMatchString=cpe:/{cve_query_string}"
 
-        response = requests.get(url)
+    response = requests.get(url)
+    
+    try:
         data = response.json()
+    except json.JSONDecodeError:
+        print(colored(f"Error decoding JSON for CPE: {cpe_string}. Skipping.", "red"))
+        return []
 
-        if "result" in data:
-            cves = data["result"]["CVE_Items"]
-            for cve_item in cves:
-                cve_id = cve_item["cve"]["CVE_data_meta"]["ID"]
-                snyk_short_name = synk_db(cve_id)
+    if "result" in data:
+        cves = data["result"]["CVE_Items"]
+        for cve_item in cves:
+            cve_id = cve_item["cve"]["CVE_data_meta"]["ID"]
+            snyk_short_name = synk_db(cve_id)
 
-                description = cve_item["cve"]["description"]["description_data"][0]["value"]
-                link = f"https://nvd.nist.gov/vuln/detail/{cve_id}"
+            description = cve_item["cve"]["description"]["description_data"][0]["value"]
+            link = f"https://nvd.nist.gov/vuln/detail/{cve_id}"
 
-                weaknesses = []
-                if "problemtype" in cve_item["cve"]:
-                    for problem_type in cve_item["cve"]["problemtype"]["problemtype_data"]:
-                        for description in problem_type["description"]:
-                            weaknesses.append(description["value"])
+            weaknesses = []
+            if "problemtype" in cve_item["cve"]:
+                for problem_type in cve_item["cve"]["problemtype"]["problemtype_data"]:
+                    for description in problem_type["description"]:
+                        weaknesses.append(description["value"])
 
-                if "description_data" in cve_item["cve"]["description"]:
-                    description_text = cve_item["cve"]["description"]["description_data"][0]["value"]
-                else:
-                    description_text = "Description not available."
+            if "description_data" in cve_item["cve"]["description"]:
+                description_text = cve_item["cve"]["description"]["description_data"][0]["value"]
+            else:
+                description_text = "Description not available."
 
-                cve_details = {
-                    "CVE ID": cve_id,
-                    "Short Name": snyk_short_name,
-                    "Description": description_text,
-                    "Weaknesses": ", ".join(weaknesses),
-                    "Link": link
-                }
+            # Check for public exploit using pyExploitDb
+            pEdb = PyExploitDb()
+            pEdb.debug = False
+            pEdb.openFile()
+            exploit_status = pEdb.searchCve(cve_id)
+            if exploit_status:
+                exploit_status = "Public Exploit Found"
+            else:
+                exploit_status = "No Public Exploit Found"
 
-                results.append(cve_details)
+            cve_details = {
+                "CVE ID": cve_id,
+                "Short Name": snyk_short_name,
+                "Description": description_text,
+                "Weaknesses": ", ".join(weaknesses),
+                "Link": link,
+                "Exploit Status": exploit_status
+            }
+
+            results.append(cve_details)
 
     return results
 
@@ -95,32 +112,21 @@ if __name__ == "__main__":
         for cpe_string in cpe_strings:
             print(colored(f"  {cpe_string}", "green"))
         
-        export_option = input(colored("\nDo you want to export results to a text document? (yes/no): ", "yellow"))
-        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        filename = f"{component}_{version}_{timestamp}.txt"
-        
-        if export_option.lower() == "yes":
-            results = fetch_cve_details(cpe_strings)
-            with open(filename, "w") as f:
+        for cpe_string in cpe_strings:
+            results = fetch_cve_details(cpe_string)
+            if results:
+                print(colored("\nCVE Details", "cyan", attrs=["underline"]))
                 for result in results:
-                    f.write(f"CVE ID: {result['CVE ID']}\n")
+                    print(colored(f"CVE ID: {result['CVE ID']}", "white"))
                     if result["Short Name"]:
-                        f.write(f"Short Name: {result['Short Name']}\n")
-                    f.write(f"Description: {result['Description']}\n")
+                        print(colored(f"Short Name: {result['Short Name']}", "light_blue"))
+                    print(colored(f"Description: {result['Description']}", "yellow"))
                     if result["Weaknesses"]:
-                        f.write(f"Weaknesses: {result['Weaknesses']}\n")
-                    f.write(f"Link: {result['Link']}\n\n")
-            print(colored(f"Results exported to '{filename}'", "green"))
-        
-        results = fetch_cve_details(cpe_strings)
-        for result in results:
-            print(colored("\nCVE Details", "cyan", attrs=["underline"]))
-            print(colored(f"CVE ID: {result['CVE ID']}", "red"))
-            if result["Short Name"]:
-                print(colored(f"Short Name: {result['Short Name']}", "green"))
-            print(colored(f"Description: {result['Description']}", "yellow"))
-            if result["Weaknesses"]:
-                print(colored(f"Weaknesses: {result['Weaknesses']}", "magenta"))
-            print(colored(f"Link: {result['Link']}\n", "blue"))
-    else:
+                        print(colored(f"Weaknesses: {result['Weaknesses']}", "magenta"))
+                    print(colored(f"Link: {result['Link']}", "blue"))
+                    if result["Exploit Status"] == "Public Exploit Found":
+                        print(colored(f"Exploit Status: {result['Exploit Status']}\n", "red"))
+                    else:
+                        print(colored(f"Exploit Status: {result['Exploit Status']}\n", "green"))
+    else:               
         print(colored("CPEs not found for the provided component and version.", "red"))
